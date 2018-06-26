4802543f03563056fd5fc8d6f8d8cd3f11dea4de
Sheroyi <39559716+Sheroyi@users.noreply.github.com>
2018-06-27 02:18:03 +0800

    Update doubangame.py

--- doubangame.py
+++ doubangame.py
@@ -5,14 +5,16 @@ import pandas
 url = 'https://www.douban.com/j/ilmen/game/search?genres=&platforms=94&q=&sort=rating&more={}'
 def parseGameList(url):
     newgamelist = []
-    for i in range(1,501): #页数
-        newurl = url.format(i)
+    n = 1
+    for i in range(n): #页数
+        newurl = url.format(i+1)
         res = requests.get(newurl)
         jd = json.loads(res.text)
         for cnt in jd['games']: #每个项目
             newgamelist.append(cnt)
-        print(i)
+        p = round((i+1) * 100/n)
+        print("\r进度:{0}%".format(p), end='', flush=True)
     return newgamelist
 newgame = parseGameList(url)
 df = pandas.DataFrame(newgame)
-df.to_excel('newgame.xlsx')
+df.to_excel('gamelist.xlsx')
