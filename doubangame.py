ca3db97e2941576f1f594e8589cb13cbc55fd188
Sheroy <39559716+Sheroyi@users.noreply.github.com>
2019-04-09 21:56:44 +0800

    Update doubangame.py

--- doubangame.py
+++ doubangame.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-import pandas
+import pandas as pd
+print('请输入抓取页数：')
+n = int(input())
 url = 'https://www.douban.com/j/ilmen/game/search?genres=&platforms=94&q=&sort=rating&more={}'
-def parseGameList(url):
+def parseGameList(url,n):
     newgamelist = []
-    n = 1
     for i in range(n): #页数
         newurl = url.format(i+1)
         res = requests.get(newurl)
@@ -13,8 +14,10 @@ def parseGameList(url):
         for cnt in jd['games']: #每个项目
             newgamelist.append(cnt)
         p = round((i+1) * 100/n)
-        print("\r进度:{0}%".format(p), end='', flush=True)
+        print("\r抓取进度:{0}%".format(p), end='', flush=True)
+        if(i == (n-1)):
+            print("\n完成！")
     return newgamelist
-newgame = parseGameList(url)
-df = pandas.DataFrame(newgame)
+newgame = parseGameList(url,n)
+df = pd.DataFrame(newgame)
 df.to_excel('gamelist.xlsx')
